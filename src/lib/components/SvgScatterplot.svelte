2439443aeafc91189c173a2f1a032d16432e8451
John Evans <john@wearelighthouse.com>
2023-03-31 01:32:47 +0100

    Extra compression techniques & set defauly precision to 2

--- src/lib/components/SvgScatterplot.svelte
+++ src/lib/components/SvgScatterplot.svelte
@@ -8,16 +8,25 @@
 	export let data;
 	export let x = default_x;
 	export let y = default_y;
-	export let precision = 4;
+	export let precision = 2;
+
+	let prev_x = 0;
+	let prev_y = 0;
 
 	$: d = data
 		.map((d, i) => {
 			const _x = $x_scale(x(d, i));
 			const _y = $y_scale(y(d, i));
+			const prev_rounded_diff_x = prev_x - round(prev_x, precision);
+			const prev_rounded_diff_y = prev_y - round(prev_y, precision);
+			const __x = round(_x - prev_x + prev_rounded_diff_x, precision);
+			const __y = round(_y - prev_y + prev_rounded_diff_y, precision);
+			prev_x = _x;
+			prev_y = _y;
 
-			return `M${round(_x, precision)} ${round(_y, precision)} A0 0 0 0 1 ${round(_x + 0.0001, precision)} ${round(_y + 0.0001, precision)}`;
+			return `m${__x}${__y < 0 ? '' : ' '}${__y}h0`;
 		})
-		.join(' ');
+		.join('');
 </script>
 
 <slot {d} />
